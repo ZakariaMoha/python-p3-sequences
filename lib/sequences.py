e73311aadde5b3396676327122058b3dc4971307
ZakariaMoha <zakmi.moha@gmailcom>
2023-11-28 09:08:25 +0300

    done lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print("[]")
+    elif n == 1:
+        print("[0]")
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, n):
+            next_number = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_number)
+        print(f"{fib_sequence}")
